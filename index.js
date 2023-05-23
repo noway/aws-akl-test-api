b2b1277e443cefa3a20d4c02a5ed837ddcf9f6b6
Ilia Sidorenko <ilia.sidorenko.421@gmail.com>
2023-05-24 00:41:46 +1200

    not only hello should add ip:port to the client list

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const dgram = require("dgram");
 const server = dgram.createSocket("udp4");
 require("dotenv").config();
 
-const clients = [];
+const clients = new Map();
 
 function decode(buffer) {
   var x = buffer.readFloatBE(1);
@@ -18,12 +18,13 @@ server.on("error", (err) => {
 server.on("message", (msg, rinfo) => {
   if (msg[0] === 1) {
     console.log(`server got: hello from ${rinfo.address}:${rinfo.port}`);
-    clients.push([rinfo.address, rinfo.port]);
+    clients.set(rinfo.address + rinfo.port, [rinfo.address, rinfo.port])
   } else {
+    clients.set(rinfo.address + rinfo.port, [rinfo.address, rinfo.port])
     const coords = decode(msg);
-    console.log(`${coords[0].toFixed(3)}, ${coords[1].toFixed(3)}`);
-    for (var i = 0; i < clients.length; i++) {
-      var client = clients[i];
+    console.log(`${coords[0].toFixed(3)}, ${coords[1].toFixed(3)}`)
+    const clientsValues = clients.values()
+    for (const client of clientsValues) {
       server.send(msg, client[1], client[0]);
     }
   }
